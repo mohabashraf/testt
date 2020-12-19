0a40d34aae81a8aa14f9fadcc2108b7794507642
mohabashraf <mohabashraf466@gmail.com>
2020-12-19 18:12:07 +0200

    Add the routing

---
+++ src/trade.js
@@ -0,0 +1,14 @@
+const express = require('express');
+// const userRouter = require('./controller/user');
+// const stockRouter = require('./controller/user');
+// const transactionRouter = require('./controller/user');
+const app = express();
+
+app.use(express.json())
+// app.use('/', userRouter)
+// app.use('/', stockRouter)
+// app.use('/', transactionRouter)
+app.listen( 3000,
+             console.log(`Trade API is running port 3000`))
+
+module.exports = app;
